272cae79e3f15ae5c1047df785cd5e903ddf8f7b
Augustine Joseph <augustine@reckonsys.com>
2025-11-08 21:08:23 +0530

    Updated the base in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/gh_pages/',
+  base: '/github_pages/',
   build: {
     outDir: 'docs',
     emptyOutDir: true,
